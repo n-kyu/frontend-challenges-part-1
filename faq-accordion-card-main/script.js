a1b310a4305f0eb91ad89ed086227f6bbfa7b7f4
Nathan Costa <nkyucode@gmail.com>
2023-08-07 09:31:37 -0300

    ✨ feat: adding button to show content of each FAQ

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,1 +1,6 @@
-
+document.querySelectorAll(".accordion__question").forEach((item) => {
+    item.addEventListener("click", (event) => {
+        item.classList.toggle("show");
+        item.nextElementSibling.classList.toggle("show");
+    });
+});
